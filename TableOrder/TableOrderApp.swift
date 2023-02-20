ba64c23a9a6a971ae9689340db25689a8164455b
AtheerSaadA <atheerrsaad@gmail.com>
2023-02-20 21:31:59 +0300

    Initial Commit

---
+++ TableOrder/TableOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableOrderApp.swift
+//  TableOrder
+//
+//  Created by AtheerAlshehri on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TableOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
